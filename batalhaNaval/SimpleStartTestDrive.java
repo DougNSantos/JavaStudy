80a5deb983d76882d25e540416727b16882e3215
Douglas Noll dos Santos <dougnsxd@gmail.com>
2025-09-22 21:15:45 -0300

    escrito código simples da batalha naval em Java

--- batalhaNaval/SimpleStartTestDrive.java
+++ batalhaNaval/SimpleStartTestDrive.java
@@ -2,6 +2,13 @@ package batalhaNaval;
 
 public class SimpleStartTestDrive {
 
+    //Essa assinatura do método main() em Java sempre será com esses argumentos (String[] args)
+    //Isso se dá porque o java permite que usemos essa String[] de array chamada args em nosso código
+    //Assim podemos chamar nosso programa e já passar parametros para ele, normalmente em terminal é usado isso
+    //Mas mesmo que não usemos, o Java vai esperar essa assinatura para rodar nosso código!
+    //ex usando dentro de nosso código: String[] vetor1 = new String(args[5]); 
+    //--> estou passando o indice [5] de meu array de Strings chamado args que vem de fora de meu programa e já estou...
+    //passando para dentro de meu programa
     public static void main(String[] args){
 
         InicioSimples start = new InicioSimples();
