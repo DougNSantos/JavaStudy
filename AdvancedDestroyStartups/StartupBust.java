9e5877a9f750272893ca699f331a86c7db6b3065
Douglas Noll dos Santos <dougnsxd@gmail.com>
2025-10-25 18:48:58 -0300

    esestruturado arquivos e terminado advanced batalha naval

--- AdvancedDestroyStartups/StartupBust.java
+++ AdvancedDestroyStartups/StartupBust.java
@@ -22,8 +22,56 @@ public class StartupBust {
         System.out.println("Seu desafio é destruir 3 startups");
         System.out.println("TECHSAT, YOKO e MANDRIL");
         System.out.println("Bom Game!");
+    
+
+        for (Startup startup : startups){
+            ArrayList<String> newLocation = helper.placeStartup(3);
+            startup.configPosicoes(newLocation);
+        }
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////
+    private void startPlaying(){
+        while (!startups.isEmpty()){
+            String userGuess = helper.getUserInput("Digite um chute");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+
+    private void checkUserGuess(String userGuess){
+        numOfGuesses++;
+        String result = "errou";
+
+        for (Startup startupToTest : startups){
+            result = startupToTest.palpite(userGuess);
+
+            if (result.equals("acertou")){
+                break;
+            }
+            if (result.equals("destruiu")){
+                startups.remove(startupToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame(){
+        System.out.println("Todas as startups foram destruidas");
+        if(numOfGuesses <= 18){
+            System.out.println("somente te tomou: " + numOfGuesses);
+
+        }
+        else{
+            System.out.println("te tomou bastante: " + numOfGuesses);
+
+        }
+    }
+
+    public static void main(String[] args){
+        StartupBust novoJogo = new StartupBust();
+        novoJogo.setUpGame();
+        novoJogo.startPlaying();
+    }
 
 }
